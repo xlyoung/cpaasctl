5b19fe5a4d424842bce72f1096d9cd7c742bc192
yangzhuohua <yangzhuohua@cyg.com>
2023-11-06 17:33:53 +0800

    增加版本信息

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "gitlab.hycyg.com/paas-tools/cpaasctl/cmd"
+import (
+	"gitlab.hycyg.com/paas-tools/cpaasctl/cmd"
+	"gitlab.hycyg.com/paas-tools/cpaasctl/internal/logger"
+)
+
+var version string
 
 func main() {
+	logger.Logger.Infof("version: %s", version)
 	cmd.Execute()
 }
